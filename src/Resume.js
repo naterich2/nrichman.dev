000d31a1f647903f8fe884022eeb4df6f5aff49a
Nate Richman <nate@nrichman.dev>
2019-07-21 17:51:46 -0500

    Added resume page

--- src/Resume.js
+++ src/Resume.js
@@ -5,23 +5,58 @@ import Live from './Live.js'
 import MainNav from './MainNav.js'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
 
-function Home() {
+function Resume() {
   return (
     <div className="home">
 	    <div style={{position:'fixed', backgroundAttachment: 'scroll', top:'50%', width:'100%', backgroundColor: 'rgb(100,100,100)'}}>
 	      <MainNav />
         <Jumbotron style={{backgroundAttachment: 'scroll', position: 'fixed', left: '15%', width:'70%'}}>
-          <Container style={{width:'50%'}}>
+          <Container style={{width:'80%'}}>
             <Row>
-              <Col md={{span:'2',offset:'5'}}>
-                <Image src='/public/Nate.jpg' roundedCircle />
+              <Col md={{span: 2, offset=5}}>
+                <h3>Nate Richman</h3>
+                <a href="mailto:nate@nrichman.dev">Contact</a>
               </Col>
             </Row>
             <Row>
-              <h3>Nate Richman</h3>
-              <p>Hey I'm Nate!  I'm a Masters student UW-Madison in Biomedical Engineering.  I did my undergrad in Biomedical Engineering and minored in Comp. Sci., Biology in Engineering, and Honors, research-based biology.</p>
-              <p>I love to learn and discover new things so I work on a lot of projects outside of work or school.  Most recently I'm really interested in computer networking from my time as a network engineer at the Department of Information Technology (DoIT) on campus.  Over the past few years I've been accumulating hardware and implementing software to make my own networking lab with which to experiment, including building this website!</p>
-              <p>Outside of school and nerdy computer things, I love to play, <a href="https://www.last.fm/user/naterich2">listen to</a>, and <a href="https://rateyourmusic.com/~naterich2">rate</a> music.  I love to bike, rock climb, camp, canoe, hike, kayak, and many other outdoors things.</p>
+              <Col md={{span: 2}}>
+                <h4><b>Objective</b></h4>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={{span: 12}}>
+                <p>I am an undergraduate student looking to contribute to biomedical research and development in a technical setting. I
+    am fascinated by many areas of biomedical engineering including electronics, mechanics, biochemistry, and computer
+    science/informatics. In order to learn more in these fields I work as a network engineer in the Division of IT at UW-
+    Madison and take computer science classes for a minor, I worked in a mechanical engineering lab, and I major in
+    Biomedical Engineering. I love to learn, and challenging problems inspire my curiosity, because of this I am a quick
+    learner, and I have demonstrated my ability to work in a fast paced environment.</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={{span: 2}}>
+                <h4><b>Education</b></h4>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={{span: 8}}>
+                <p><b>Bachelor of Biomedical Engineering, emphasis in Biomaterials, Cellular/Tissue Engineering</b></p>
+                <p>Certificates in Biocore Honors, Computer Science, and Biology in Engineering</p>
+                <p><i>Univeresity of Wisconsin-Madison</i>, Madison, WI</p>
+                <p>&nbsp&nbsp&nbsp&nbsp Graduated with Highest Distinction</p>
+              </Col>
+              <Col md={{span:2, offset:10}}>
+                <p>May 2019</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={{span: 8}}>
+                <p><b>Master of Biomedical Engineering</b></p>
+                <p><i>Univeresity of Wisconsin-Madison</i>, Madison, WI</p>
+              </Col>
+              <Col md={{span:2, offset:10}}>
+                <p>May 2020</p>
+              </Col>
             </Row>
           </Container>
         </Jumbotron>
