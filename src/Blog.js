389a8c84a223babf1ff02ee8740db7c838132106
Nate Richman <nate@nrichman.dev>
2019-07-24 07:45:56 -0500

    Refactor Blog component to have class so it can fetch data with componentDidMount()

--- src/Blog.js
+++ src/Blog.js
@@ -6,22 +6,71 @@ import MainNav from './MainNav.js'
 import Footer from './Footer.js'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
 
-function Blog() {
-  return (
-    <div className="home">
-	    <Live />
-	    <div style={{position:'absolute', backgroundAttachment: 'scroll', top:'70%', width:'100%', backgroundColor: '#282c35'}}>
-	      <MainNav />
-        <Carousel>
-          {/* Put have 5 most recent blog posts circulating in carousel */}
-        </Carousel>
-        <ButtonGroup>
-          {/* Have a button for all the tags */}
-        </ButtonGroup>
-        <Footer />
-	    </div>
-    </div>
-  );
-}
+class Blog extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      blogs: null,
+      isLoading: true
+    }
+  }
+  componentDidMount(){
+    fetch('/resources/blog/recent')
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        this.setState({blogs: data, isLoading: false});
+      });
+  }
 
+  render(){
+    if(this.state.isLoading) return (<></>)
+    else {
+      const items = [];
+      for (const [index,value] of this.state.blogs.entries()){
+        items.push(
+          <Carousel.Item>
+            <Container>
+              <Row>
+                <Col md={{span:4, offset:4}}>
+                  <h3 style={{color:"rgb(220,220,220)"}}>{value.title}</h3>
+                </Col>
+              </Row>
+            </Container>
+            <Carousel.Caption>
+              <Container>
+                <Row>
+                  <Col md={{span:4, offset:4}}>
+                    <p>{value.synopsis}</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={{span:4, offset:4}}>
+                    <p>By: {value.author}</p>
+                  </Col>
+                </Row>
+              </Container>
+            </Carousel.Caption>
+          </Carousel.Item>
+        )
+      }
+      return (
+        <div className="home">
+          <Live />
+          <div style={{position:'absolute', backgroundAttachment: 'scroll', top:'70%', width:'100%', backgroundColor: '#282c35'}}>
+            <MainNav />
+            <Carousel>
+              {items}
+            </Carousel>
+            <ButtonGroup>
+              {/* Have a button for all the tags */}
+            </ButtonGroup>
+            <Footer />
+          </div>
+        </div>
+      );
+    }
+  }
+}
 export default Blog;
