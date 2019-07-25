4496c67868d9757647de9b612e8d82a623309ba1
Nate Richman <nate@nrichman.dev>
2019-07-25 13:15:16 -0500

    Fixes and cosmetics

--- src/BlogForm.js
+++ src/BlogForm.js
@@ -1,5 +1,5 @@
 import './Home.css';
-import { Modal, Form, Button} from 'react-bootstrap';
+import { Modal, Form, Button, Container, Row, Col} from 'react-bootstrap';
 import React from 'react';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import ReactMarkdown from 'react-markdown';
@@ -25,30 +25,56 @@ class BlogForm extends React.Component {
             {! this.props.newBlog && <Modal.Title>Add Comment</Modal.Title>}
           </Modal.Header>
           <Modal.Body>
-            <Form>
-              <Form.Group controlId="title">
-                <Form.Label>Title</Form.Label>
-                <Form.Control placeholder="Blog" id="blog-title" />
-              </Form.Group>
-              <Form.Group controlId="synopsis">
-                <Form.Label>Summary</Form.Label>
-                <Form.Control as="textarea" rows="3" id="blog-synopsis" />
-              </Form.Group>
-              <Form.Group controlId="beginning">
-                <Form.Label>Preview</Form.Label>
-                <Form.Control as="textarea" rows="3" id="blog-beginning" />
-              </Form.Group>
-              <Form.Group controlId="tags">
-                <Form.Label>Tags</Form.Label>
-                <Form.Control as="textarea" rows="2" id="blog-tags" />
-              </Form.Group>
-              <Form.Group controlId="fulltext">
-                <Form.Label>Content</Form.Label>
-                <Form.Control as="textarea" defaultValue={''} rows="6" id="blog-fulltext" onChange={() => this.setState({open: this.state.open, fulltext: document.getElementById('blog-fulltext').value})} />
+            <Container>
+              <Form>
+                <Row>
+                  <Col md={{span:8, offset: 2}}>
+                    <Form.Group controlId="title">
+                      <Form.Label>Title</Form.Label>
+                      <Form.Control placeholder="Blog" id="blog-title" />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={{span:8, offset: 2}}>
+                    <Form.Group controlId="synopsis">
+                      <Form.Label>Summary</Form.Label>
+                      <Form.Control as="textarea" rows="3" id="blog-synopsis" />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={{span:8, offset: 2}}>
+                    <Form.Group controlId="beginning">
+                      <Form.Label>Preview</Form.Label>
+                      <Form.Control as="textarea" rows="3" id="blog-beginning" />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={{span:8, offset: 2}}>
+                    <Form.Group controlId="tags">
+                      <Form.Label>Tags</Form.Label>
+                      <Form.Control as="textarea" rows="2" id="blog-tags" />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md={{span:8, offset: 2}}>
+                    <Form.Group controlId="fulltext">
+                      <Form.Label>Content</Form.Label>
+                      <Form.Control as="textarea" value={this.state.fulltext} rows="6" id="blog-fulltext" onChange={() => this.setState({open: this.state.open, fulltext: document.getElementById('blog-fulltext').value})} />
+                    </Form.Group>
+                  </Col>
+                </Row>
 
-              </Form.Group>
-            </Form>
-            <ReactMarkdown source={this.state.fulltext} />
+              </Form>
+              <Row>
+                <Col md={12}>
+                  <ReactMarkdown source={this.state.fulltext} />
+                </Col>
+              </Row>
+            </Container>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={() => this.props.close()}>Close</Button>
