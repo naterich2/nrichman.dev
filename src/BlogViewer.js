e5b8189409c2e45ffab9751b7269ebb72cacadb9
Nate Richman <nate@nrichman.dev>
2019-07-29 08:23:31 -0500

    BlogViewer works when URL is typed in

--- src/BlogViewer.js
+++ src/BlogViewer.js
@@ -1,24 +1,40 @@
 import './Home.css';
 import { Jumbotron } from 'react-bootstrap';
 import React from 'react'
+import PropTypes from 'prop-types'
 import MainNav from './MainNav.js'
 import Live from './Live.js'
 import Footer from './Footer.js'
+import CodeBlock from './subs/CodeBlock.js'
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
 import ReactMarkdown from 'react-markdown'
 
 class BlogViewer extends React.Component {
  // const my_id = this.props.match.params.id;
+  constructor(props){
+    super(props);
+    this.state = {
+      fulltext: ''
+    }
+  }
+  componentDidMount(){
+    fetch('/resources/blog/blog_id/'+this.props.match.params.id)
+      .then(resp => {
+        return resp.json();
+      })
+      .then(blog => {
+        this.setState({ fulltext: blog.full_text })
+      })
+  }
   render(){
-    console.log(this.props.match.id);
-  const mark = "# My first markdown!";
+    console.log(this.props.match.params.id);
     return (
       <div className="blog">
         <Live />
         <div style={{position:'absolute', backgroundAttachment: 'scroll', top:'70%', width:'100%', backgroundColor: '#282c35'}}>
           <MainNav />
           <Jumbotron style={{backgroundAttachment: 'scroll', position: 'relative', left: '15%', width:'70%'}}>
-            <ReactMarkdown source={mark} />
+            <ReactMarkdown source={this.state.fulltext} renderers={{code: CodeBlock}}/>
           </Jumbotron>
           <Footer />
         </div>
@@ -27,4 +43,10 @@ class BlogViewer extends React.Component {
   }
 }
 
-export default BlogViewer;
+BlogViewer.propTypes = {
+  match: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
+}
+
+export default BlogViewer
